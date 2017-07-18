b228d07d156e7cb8c2a27c0017a75c1054c8e6a5
loganbwu <loganbwu@live.com>
2017-07-19 08:22:48 +1200

    wip

--- main.py
+++ main.py
@@ -9,16 +9,29 @@ import pandas as pd
 from scrape import scrape
 import matplotlib.pyplot as plt
 
+yesterday = '7-18-2017'
+today = '7-19-2017'
 
 def interpolate_dates(dates, n=3):
+    """
+    n       : number of sub-intervals i.e. 3 months per quarter
+    """
+    
     interp_dates = []
     for i in range(len(dates)-1):
-        for j in range(1,n+1):
+        for j in range(1,n):
             delta = dates[i+1] - dates[i]
-            interp_dates.append(dates[i] + delta*j/4)
+            interp_dates.append(dates[i] + delta*j/n)
     return interp_dates
 
 def interpolate_data(df, n, key='Expiry', method='cubic'):
+    """
+    df      : data frame to be interpolated
+    n       : number of sub-intervals i.e. 3 months per quarter
+    key     : axis to interpolate over
+    method  : interpolation type
+    """
+    
     interp_dates = interpolate_dates(df[key], 3)
     interp_data = pd.DataFrame({key: interp_dates}, index = range(len(interp_dates)))
     interp_data = interp_data.append(df)
@@ -28,17 +41,32 @@ def interpolate_data(df, n, key='Expiry', method='cubic'):
     return interp_data
     
 
-date = '7-18-2017'
-
-monthly_data = scrape('monthly', date)
-quarterly_data = scrape('quarterly', date)
-
-interpolated_quarterly_data = interpolate_data(quarterly_data, 3)
-
-fig, ax = plt.subplots()
-ax.plot_date(monthly_data['Expiry'], monthly_data['Previous Settlement'], 'x-')
-ax.plot_date(quarterly_data['Expiry'], quarterly_data['Previous Settlement'], 'x-')
-ax.plot_date(interpolated_quarterly_data['Expiry'], interpolated_quarterly_data['Previous Settlement'], '-')
-ax.legend(['Monthly', 'Quarterly', 'Interpolated quarterly'])
-plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
-plt.title('Expected Otahuhu Base Price')
+def main():
+    monthly_data_yesterday = scrape('monthly', today)
+    print(monthly_data_yesterday.reset_index())
+#    quarterly_data_today = scrape('quarterly', today)
+#    interpolated_quarterly_data_today = interpolate_data(quarterly_data_today, 3)
+#    
+#    monthly_data_yesterday = scrape('monthly', yesterday)
+#    quarterly_data_yesterday = scrape('quarterly', yesterday)
+#    interpolated_quarterly_data_yesterday = interpolate_data(quarterly_data_yesterday, 3)
+    
+#    delta = interpolated_quarterly_data_today['Previous Settlement'] - interpolated_quarterly_data_yesterday['Previous Settlement']
+    
+    fig, axarr = plt.subplots(2, sharex=True)
+    axarr[0].plot_date(monthly_data_yesterday.reset_index()['Expiry'], monthly_data_yesterday[0], 'x-')
+#    axarr[0].plot_date(quarterly_data_yesterday['Expiry'], quarterly_data_yesterday['Previous Settlement'], 'x-')
+#    axarr[0].plot_date(interpolated_quarterly_data_yesterday['Expiry'], interpolated_quarterly_data_yesterday['Previous Settlement'], '-')
+    axarr[0].legend(['Monthly', 'Quarterly', 'Interpolated quarterly'])
+    axarr[0].set_title('Expected Otahuhu Base Price')
+    
+#    axarr[1].plot_date(monthly_data_yesterday['Expiry'], delta, 'x-')
+#    axarr[1].plot_date(monthly_data_today['Expiry'], monthly_data_today['Previous Settlement'], 'x-')
+#    axarr[1].plot_date(quarterly_data_today['Expiry'], quarterly_data_today['Previous Settlement'], 'x-')
+#    axarr[1].plot_date(interpolated_quarterly_data_today['Expiry'], interpolated_quarterly_data_today['Previous Settlement'], '-')
+#    axarr[1].legend(['Monthly', 'Quarterly', 'Interpolated quarterly'])
+#    axarr[1].set_title('Expected Otahuhu Base Price')
+#    fig.autofmt_xdate()
+    
+if __name__ == '__main__':
+    main()
--- scrape.py
+++ scrape.py
@@ -9,14 +9,21 @@ import pandas as pd
 
 
 def scrape(period=None, date=None):
+    """
+    period  : 'monthly' or 'quarterly'
+    date    : format %m-%d-%Y
+    """
     
     filename = period + '_' + date + '.xlsx'
     df = pd.read_excel(filename, sheetname='Sheet1')
-    df = df[['Expiry', 'Previous Settlement']]
+    df = df.set_index('Expiry', drop=True)
+    df = df['Previous Settlement']
+    df.name = period + '_' + date
     
     if period == 'quarterly':
         df['Expiry'] = df['Expiry'] - dt.timedelta(days=45)
     return df
+    df['Expiry'] = pd.DatetimeIndex(df['Expiry']).round('D')
 
 if __name__ == '__main__':
     raise Exception('Don\'t run this file')
